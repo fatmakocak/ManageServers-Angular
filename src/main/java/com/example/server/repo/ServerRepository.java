3f3c693673a201e116187b2120033c0137ae6df7
fatma.kocak <fatma.kocak@vbt.com.tr>
2022-07-22 14:21:38 +0300

    service package oluşturuldu.

--- src/main/java/com/example/server/repo/ServerRepository.java
+++ src/main/java/com/example/server/repo/ServerRepository.java
@@ -1,4 +1,9 @@
 package com.example.server.repo;
 
-public class ServerRepository {
+import com.example.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepository extends JpaRepository<Server,Long> {
+    Server findByIpAddress (String ipAddress);
+
 }
