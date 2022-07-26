0938e60d0fcf7b8b9a7028cb21ad014d8a8eaf4d
fatma.kocak <fatma.kocak@vbt.com.tr>
2022-07-26 11:01:41 +0300

    controller tamam.

--- src/main/java/com/example/server/model/Server.java
+++ src/main/java/com/example/server/model/Server.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Data
-@Table(name = "server")
+@Table(name = "images")
 @AllArgsConstructor
 @NoArgsConstructor
 
--- src/main/java/com/example/server/service/Impl/ServerServiceImpl.java
+++ src/main/java/com/example/server/service/Impl/ServerServiceImpl.java
@@ -4,16 +4,18 @@ import com.example.server.model.Server;
 import com.example.server.model.Status;
 import com.example.server.repo.ServerRepository;
 import com.example.server.service.ServerService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Random;
 
 @RequiredArgsConstructor
 @Service
@@ -26,8 +28,8 @@ public class ServerServiceImpl implements ServerService {
     @Override
     public Server create(Server server) {
         log.info("saving new server:{}", server.getName());
-       server.setImageUrl(setServerImageUrl());
-       return serverRepository.save(server);
+        server.setImageUrl(setServerImageUrl());
+        return serverRepository.save(server);
     }
 
 
@@ -35,16 +37,16 @@ public class ServerServiceImpl implements ServerService {
     public Collection<Server> list(int limit) {
         log.info("fetching all servers");
 
-        return serverRepository.findAll(PageRequest.of(0,limit)).toList();
+        return serverRepository.findAll(PageRequest.of(0, limit)).toList();
 
     }
 
     @Override
     public Server ping(String ipAddress) throws IOException {
-        log.info("pinging server IP:{}",ipAddress);
+        log.info("pinging server IP:{}", ipAddress);
         Server server = serverRepository.findByIpAddress(ipAddress);
         InetAddress address = InetAddress.getByName(ipAddress);
-        server.setStatus(address.isReachable(10000) ? Status.SERVER_UP:Status.SERVER_DOWN);
+        server.setStatus(address.isReachable(10000) ? Status.SERVER_UP : Status.SERVER_DOWN);
         serverRepository.save(server);
         return server;
 
@@ -52,7 +54,7 @@ public class ServerServiceImpl implements ServerService {
 
     @Override
     public Server get(Long id) {
-        log.info("Fetching server by id: {}",id);
+        log.info("Fetching server by id: {}", id);
         return serverRepository.findById(id).get();
 
     }
@@ -66,15 +68,18 @@ public class ServerServiceImpl implements ServerService {
 
     @Override
     public Boolean delete(Long id) {
-        log.info("delete server by id: {}",id);
+        log.info("delete server by id: {}", id);
 
-         serverRepository.deleteById(id);
-         return true;
+        serverRepository.deleteById(id);
+        return true;
 
 
     }
+
     private String setServerImageUrl() {
-        return null;
-    }
 
+        String[] imageNames = {"server1.jpg", "server2.png", "server3.png", "server4.png", "server5.png"};
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/server/images" + imageNames[new Random().nextInt(4)]).toUriString();
+
+    }
 }
